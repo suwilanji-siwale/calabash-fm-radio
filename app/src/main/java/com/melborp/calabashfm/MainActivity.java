29e09ed3a7dfb86fc045a625bdd3f823045a14fe
suwilanji <suwilanjisiwale1@gmail.com>
2021-04-12 01:10:07 +0200

    First Commit

---
+++ app/src/main/java/com/melborp/calabashfm/MainActivity.java
@@ -0,0 +1,20 @@
+package com.melborp.calabashfm;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
